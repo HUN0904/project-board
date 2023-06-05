eaf0fd57af1cb69d0cdcddd96a2e31009ff764bc
HUN0904 <sh930904@naver.com>
2023-06-05 22:15:43 +0900

    #16 - 버전3.0 -> 2.7 변경

--- src/main/java/com/example/projectboard/config/ThymeleafConfig.java
+++ src/main/java/com/example/projectboard/config/ThymeleafConfig.java
@@ -1,11 +1,13 @@
 package com.example.projectboard.config;
 
+
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
-
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
 public class ThymeleafConfig {
@@ -21,17 +23,16 @@ public class ThymeleafConfig {
     }
 
 
+    @RequiredArgsConstructor
     @Getter
+    @ConstructorBinding
     @ConfigurationProperties("spring.thymeleaf3")
     public static class Thymeleaf3Properties {
         /**
          * Use Thymeleaf 3 Decoupled Logic
          */
         private final boolean decoupledLogic;
-
-        public Thymeleaf3Properties(boolean decoupledLogic) {
-            this.decoupledLogic = decoupledLogic;
-        }
     }
+
 
 }
